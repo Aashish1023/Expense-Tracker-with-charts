11da3c21a178aa5453a576a299f2e48cd79218a5
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-07-19 08:41:52 +0545

    added get user input at for add_expense

--- gui/add_expense.py
+++ gui/add_expense.py
@@ -1,4 +1,12 @@
-def add_expense(amount, category, date, note):
+def add_expense():
+    category = input("enter expense category(E.g. Food, Transport): ")
+    amount = input("enter expense amount: ")
+    date = input("Enter expense date (YYYY-MM-DD): ")
+     
+    if not date:
+       date = datetime.date.today().strftime('%Y-%m-%d')
+
+    
     db = connect_db()
     cursor = db.cursor()
     sql = "INSERT INTO expenses (amount, category, date, note) VALUES (%s, %s, %s, %s)"
