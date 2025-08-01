d23a645f4d0394a63d49bdb85ee3b7be91993843
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-08-01 19:51:57 +0545

    fix: refactor add_expense function to use GUI inputs and improve error handling

--- gui/add_expense.py
+++ gui/add_expense.py
@@ -8,31 +8,32 @@ from utils.db_connection import create_connection
 import datetime
 
 def add_expense():
-   category = input("enter expense category(E.g. Food, Transport): ")
-   amount = input("enter expense amount: ")
-   date = input("Enter expense date (YYYY-MM-DD): ")
+   date = entry_date.get()
+   category = entry_category.get()
+   amount = entry_amount.get()
+   description = entry_description.get()
+   # category = input("enter expense category(E.g. Food, Transport): ")
+   # amount = input("enter expense amount: ")
+   # date = input("Enter expense date (YYYY-MM-DD): ")
      
-   if not date:
-       date = datetime.date.today().strftime('%Y-%m-%d')
+   if not (date and category and amount):
+      messagebox.showerror("Missing Info", "Please fill in all fields.")
+      return
+      #  date = datetime.date.today().strftime('%Y-%m-%d')
 
+try: # Connect to the database
+   amount = float(amount)
    conn = create_connection()
-   if conn:
-      cursor = conn.cursor()
-      try:
-         cursor.execute(
-            "INSERT INTO expenses (category, amount, date) VALUES (%s, %s, %s)",
-            (category, amount, date)
-         )
-         conn.commit()
-         print("Expense added successfully.")
-      except Exception as e:
-         print(f"An error occurred: {e}")
-      finally:
-         cursor.close()
-         conn.close()
-   else:
-      print("Failed to connect to the database.")
-
-
-if __name__ == "__main__":
-    add_expense()
+   cursor = conn.cursor()
+   cursor.execute("INSERT INTO expenses (date, category, amount, description) VALUES (%s, %s, %s, %s)", (date, category, amount, description))
+   
+   conn.commit()
+   cursor.close()
+   conn.close()
+   messagebox.showinfo("Success", "Expense added successfully!")
+   entry_date.delete(0, tk.END)
+   entry_category.delete(0, tk.END)
+   entry_amount.delete(0, tk.END)
+   entry_description.delete(0, tk.END)
+except Exception as e:
+   messagebox.showerror("Error", str(e))
