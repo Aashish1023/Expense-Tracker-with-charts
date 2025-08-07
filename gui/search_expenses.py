8e86475153b0ec4e90985701b83639ff6dc21176
Aashish1023 <aashishmaharjan1999@gmail.com>
2025-08-07 21:30:03 +0545

    correct cursor execution and improve fetch_filtered_expenses function

--- gui/search_expenses.py
+++ gui/search_expenses.py
@@ -14,3 +14,27 @@ def create_connection():
     except mysql.connector.Error as e:
         messagebox.showerror("Database Error", str(e))
         return None
+
+
+# Fetch expenses from the database
+def fetch_filtered_expenses(date_filter=None, category_filter=None):
+    conn = create_connection()
+    if not conn:
+        return[]
+    
+    cursor = conn.cursor()
+    query = "SELECT id, date, category, amount, description FROM expenses WHERE 1=1"
+    values = []
+
+    if date_filter:
+        query += " AND date = %s"
+        values.append(date_filter)
+    if category_filter:
+        query += " AND category LIKE %s"
+        values.append('%' + category_filter + '%')
+
+    cursor, execute(query, tuple(values))
+    date = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return date
